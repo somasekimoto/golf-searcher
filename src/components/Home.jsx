fea05844891c539eb244b273fa0fd62eacf5fb75
SomaSekimoto <0421.soma@gmail.com>
2020-04-16 15:49:22 +0900

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import "./Common.css";
+import "semantic-ui-css/semantic.min.css";
+import DatePicker, { registerLocale } from "react-datepicker";
+import ja from "date-fns/locale/ja";
+import "react-datepicker/dist/react-datepicker.css";
+import addDays from "date-fns/addDays";
+import axios from "axios";
+import format from "date-fns/format";
+import Result from './Result';
+import Loading from './Loading';
+
+const Today = new Date();
+registerLocale("ja", ja);
+
+class Home extends React.Component {
+  state = {
+    date: addDays(new Date(), 14),
+    budget: "12000",
+    departure: "1",
+    duration: "90",
+    planCount: 0,
+    plans: null,
+    error: false,
+    loading: false
+  };
+
+  onFormSubmit = async (event) => {
+    try{
+      event.preventDefault();
+      this.setState({ loading:true });
+
+      const response = await axios.get("http://localhost:3000/plans", {
+        params: {
+          date: format(this.state.date, "yyyyMMdd"),
+          budget: this.state.budget,
+          departure: this.state.departure,
+          duration: this.state.duration
+        }
+      });
+      this.setState({
+        planCount: response.data.planCount,
+        plans: response.data.plans
+      })
+      this.setState({ loading: false });
+    } catch (e) {
+      this.setState({ error: e })
+    }
+  };
+
+  render() {
+    return (
+      <div className="ui container" id="container">
+        <div className="Search__Form">
+          <form className="ui form segment" onSubmit={this.onFormSubmit}>
+            <div className="field">
+              <label>
+                <i className="calendar alternate outline icon"></i>プレー日
+              </label>
+              <DatePicker
+                dateFormat="yyyy/MM/dd"
+                locale="ja"
+                selected={this.state.date}
+                onChange={e => this.setState({ date: e })}
+                minDate={Today}
+              />
+            </div>
+            <div className="field">
+              <label>
+                <i className="yen sign icon"></i>上限金額
+              </label>
+              <select
+                className="ui dropdown"
+                name="dropdown"
+                value={this.state.budget}
+                onChange={e => this.setState({ budget: e.target.value })}
+              >
+                <option value="8000">8,000円</option>
+                <option value="12000">12,000円</option>
+                <option value="16000">16,000円</option>
+              </select>
+            </div>
+            <div className="field">
+              <label>
+                <i className="map pin icon"></i>移動時間計算の出発地点（自宅から近い地点をお選びください）
+              </label>
+              <select
+                className="ui dropdown"
+                name="dropdown"
+                value={this.state.departure}
+                onChange={e => this.setState({ departure: e.target.value })}
+              >
+                <option value="1">東京駅</option>
+                <option value="2">横浜駅</option>
+              </select>
+            </div>
+            <div className="field">
+              <label>
+                <i className="car icon"></i>車での移動時間の上限
+              </label>
+              <select
+                className="ui dropdown"
+                name="dropdown"
+                value={this.state.duration}
+                onChange={e => this.setState({ duration: e.target.value })}
+              >
+                <option value="60">60分</option>
+                <option value="90">90分</option>
+                <option value="120">120分</option>
+              </select>
+            </div>
+            <div className="Search__Button">
+              <button type="submit" className="Search__Button__Design">
+                <i className="search icon"></i>ゴルフ場を検索する
+              </button>
+            </div>
+          </form>
+
+          <Loading loading={this.state.loading} />
+
+          <Result 
+            plans={this.state.plans}
+            planCount={this.state.planCount}
+            error={this.state.error}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/Result.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./Common.css";
+
+const Result = ({ plans, planCount, error }) => {
+  if (error) {
+    return (
+      <div className="wrapper">
+        <div className="ui negative message">
+          <i className="close icon"></i>
+          <div className="header">
+            エラーが発生しました。
+          </div>
+          検索条件を見直すか、管理者にお問い合わせください。
+        </div>
+      </div>
+    );
+  }
+  
+  if (!plans) {
+    return <div></div>;
+  }
+
+  if (planCount === 0) {
+    return (
+      <div className="wrapper">
+        <div className="ui orange message">
+          <div className="header">
+            ゴルフ場が見つかりませんでした。条件を変更して再度検索してください。
+          </div>
+        </div>
+      </div>
+    )
+  }
+  const results = plans.map(plan => {
+    return (
+      <div className="item" key={plan.plan_id}>
+        <div className="image">
+          <img src={plan.image_url} alt={plan.course_name} />
+        </div>
+        <div className="content">
+          <div className="meta">
+            <span className="cinema">{plan.course_name}</span>
+            <div className="ui mini statistics">
+              <div className="statistic">
+                <div className="value">
+                  <i className="car icon"></i> {plan.duration + "分"}
+                </div>
+              </div>
+              <div className="statistic">
+                <div className="value">
+                  <i className="yen sign icon"></i> {plan.price.toLocaleString()}
+                </div>
+              </div>
+              <div className="statistic">
+                <div class="value">
+                  <i className="thumbs up outline icon"></i> {plan.evaluation}
+                </div>
+              </div>
+            </div>
+            <div className="ui star rating" data-rating="3"></div>
+            <div className="extra">
+              <div className="ui label">{plan.prefecture}</div>
+              <div className="ui label">{plan.plan_name}</div>
+            </div>
+          </div>
+          <div className="description">
+            <p>{plan.caption}</p>
+          </div>
+          <div className="item-button">
+            <a
+              href={plan.reserve_url_pc}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              コースの予約はこちら
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{results}</div>
+};
+
+export default Result;
